b4e86122bd3e4be3bf9bad328e163eb607316edb
Kakkoth Vayalambron, Shibu <Shibu_KV@cable.comcast.com>
2024-12-24 11:18:59 -0800

    Add additional packages for tr69hostif
    1] libdirectfb, libtinyxml2, libyajl
    2] From source
       wrp-c, trower-64, paradous, cJson, procps-ng

--- native-platform/dependent_rdk_pkg_installer.sh
+++ native-platform/dependent_rdk_pkg_installer.sh
@@ -31,6 +31,17 @@ make -C build/rbus && make -C build/rbus install
 WORKDIR=/opt/WORKDIR
 mkdir -p $WORKDIR
 
+# cJson flavor used in RDK stack
+cd $WORKDIR
+git clone https://github.com/DaveGamble/cJSON.git
+cd cJSON
+mkdir build
+cd build
+cmake ..
+make && make install
+cd $WORKDIR
+rm -rf cJSON
+
 # Include WDMP package
 cd $WORKDIR
 git clone https://github.com/xmidt-org/wdmp-c.git
@@ -41,6 +52,39 @@ make -C build && make -C build install
 cd $WORKDIR 
 rm -rf wdmp-c
 
+cd $WORKDIR 
+git clone https://github.com/schmidtw/wrp-c.git
+git checkout main
+cd wrp-c
+mkdir build
+cd build
+cmake ..
+make && make install
+mkdir -p /usr/local/include/wrp-c
+cp -r ../src/wrp-c.h /usr/local/include/wrp-c/
+cd $ROOT
+rm -rf wrp-c
+
+cd $WORKDIR 
+git clone https://github.com/xmidt-org/libparodus.git
+cd libparodus
+mkdir build
+cd build
+cmake ..
+make && make install
+cp -r ../src/libparodus.h /usr/local/include/
+cp -r ../src/libparodus_log.h /usr/local/include/
+cd $WORKDIR 
+rm -rf libparodus
+
+
+wget https://sourceforge.net/projects/procps-ng/files/Production/procps-ng-3.3.17.tar.xz
+tar -xf procps-ng-3.3.17.tar.xz
+cd procps-3.3.17
+./configure --without-ncurses
+make && make install
+cd $WORKDIR
+rm -rf procps-ng-3.3.17.tar.xz procps-3.3.17
 
 
 #rtrouted -f -l DEBUG
