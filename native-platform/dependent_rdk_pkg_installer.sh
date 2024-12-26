989922544b45822ad08269903a70f965e547d40c
Shibu Kakkoth Vayalambron <shibu.kakkoth@gmail.com>
2024-12-26 10:27:06 -0800

    Add additional RDK dependent modules (#6)
    * Add additional RDK dependent modules
    
    * Clean up cloned wdmp source code
    
    * Add opkg required by RDM and Remote Debugger
    
    * Add additional packages for tr69hostif
    
    1] libdirectfb, libtinyxml2, libyajl
    2] From source
       wrp-c, trower-64, paradous, cJson, procps-ng
    
    * Additional modules required for tr69hostif
    
    * Removing unused packages
    
    ---------
    
    Co-authored-by: Kakkoth Vayalambron, Shibu <Shibu_KV@cable.comcast.com>

--- native-platform/dependent_rdk_pkg_installer.sh
+++ native-platform/dependent_rdk_pkg_installer.sh
@@ -28,4 +28,78 @@ git clone https://github.com/rdkcentral/rbus
 cmake -Hrbus -Bbuild/rbus -DBUILD_FOR_DESKTOP=ON -DCMAKE_BUILD_TYPE=Debug
 make -C build/rbus && make -C build/rbus install
 
+WORKDIR=/opt/WORKDIR
+mkdir -p $WORKDIR
+
+# cJson flavor used in RDK stack
+cd $WORKDIR
+git clone https://github.com/DaveGamble/cJSON.git
+cd cJSON
+mkdir build
+cd build
+cmake ..
+make && make install
+cd $WORKDIR
+rm -rf cJSON
+
+# Include WDMP package
+cd $WORKDIR
+git clone https://github.com/xmidt-org/wdmp-c.git
+cd wdmp-c
+sed -i '/WDMP_ERR_SESSION_IN_PROGRESS/a\    WDMP_ERR_INTERNAL_ERROR,\n    WDMP_ERR_DEFAULT_VALUE,' src/wdmp-c.h
+cmake -H. -Bbuild -DBUILD_FOR_DESKTOP=ON -DCMAKE_BUILD_TYPE=Debug
+make -C build && make -C build install
+cd $WORKDIR 
+rm -rf wdmp-c
+
+# Install dependencies of libparodus
+cd $WORKDIR
+git clone https://github.com/nanomsg/nanomsg.git
+cd nanomsg
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cmake --build . --target install
+cd $WORKDIR
+rm -rf nanomsg
+
+git clone https://github.com/xmidt-org/cimplog.git
+cd cimplog
+git checkout 8a5fb3c2f182241d17f5342bea5b7688c28cd1fd
+mkdir build
+cd build
+cmake ..
+make && make install
+cd $WORKDIR
+rm -rf cimplog
+
+
+cd $WORKDIR 
+git clone https://github.com/xmidt-org/wrp-c.git
+cd wrp-c
+git checkout 9587e8db33dbbfcd9b78ef66cc2eaf16dfb9afcf
+# replace  7a98138f27f27290e680bf8fbf1f8d1b089bf138 with 445880108a1d171f755ff6ac77e03fbebbb23729 in CMakeLists.txt
+# Message pack revision used in libparadous is not buildable with latest gcc versions
+sed -i 's/7a98138f27f27290e680bf8fbf1f8d1b089bf138/445880108a1d171f755ff6ac77e03fbebbb23729/g' CMakeLists.txt
+mkdir build
+cd build
+cmake ..
+make && make install
+cd $WORKDIR
+rm -rf wrp-c
+
+cd $WORKDIR 
+git clone https://github.com/xmidt-org/libparodus.git
+cd libparodus
+# replace  7a98138f27f27290e680bf8fbf1f8d1b089bf138 with 445880108a1d171f755ff6ac77e03fbebbb23729 in CMakeLists.txt
+# Message pack revision used in libparadous is not buildable with latest gcc versions
+sed -i 's/7a98138f27f27290e680bf8fbf1f8d1b089bf138/445880108a1d171f755ff6ac77e03fbebbb23729/g' CMakeLists.txt
+mkdir build
+cd build
+cmake ..
+make && make install
+cd $WORKDIR 
+rm -rf libparodus
+
 #rtrouted -f -l DEBUG
