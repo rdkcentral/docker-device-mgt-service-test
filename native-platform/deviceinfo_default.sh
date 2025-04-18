56fac5fe78228485f88619844984c3db5abd3492
madhubabutt <114217841+madhubabutt@users.noreply.github.com>
2025-04-18 23:02:03 +0530

    RDK-48829 [RDK-E] L2 test framework for tr69hostif (#69)
    Co-authored-by: mtirum011 <madhubabu_tirumala@comcast.com>

--- native-platform/deviceinfo_default.sh
+++ native-platform/deviceinfo_default.sh
@@ -2,24 +2,24 @@
 
 sed -i '/X_COMCAST-COM_STB_MAC/ {n; n; a\
         <default type="factory" value="AA:BB:CC:DD:EE:FF"/>
-}' /etc/data-model.xml
+}' /etc/data-model-stb.xml
 sed -i '/X_COMCAST-COM_STB_IP/ {n; n; a\
         <default type="factory" value="10.0.0.1"/>
-}' /etc/data-model.xml
+}' /etc/data-model-stb.xml
 sed -i '/X_COMCAST-COM_FirmwareFilename/ {n; n; a\
         <default type="factory" value="Platform_Cotainer_1.0.0"/>
-}' /etc/data-model.xml
+}' /etc/data-model-stb.xml
 sed -i '/ModelName/ {n; n; a\
         <default type="factory" value="DOCKER"/>
-}' /etc/data-model.xml
+}' /etc/data-model-stb.xml
 sed -i '/ConfigURL/ {n; n; a\
         <default type="factory" value="https://mockxconf:50050/loguploader/getT2DCMSettings"/>
-}' /etc/data-model.xml
+}' /etc/data-model-generic.xml
 sed -i '/AccountID/ {n; n; a\
         <default type="factory" value="Platform_Container_Test_DEVICE"/>
-}' /etc/data-model.xml
+}' /etc/data-model-generic.xml
 sed -i '/Telemetry.MTLS/ {n; n; n; n; d;
-}' /etc/data-model.xml
+}' /etc/data-model-generic.xml
 sed -i '/Telemetry.MTLS/ {n; n; n; a\
         <default type="factory" value="false"/>
-}' /etc/data-model.xml
+}' /etc/data-model-generic.xml
