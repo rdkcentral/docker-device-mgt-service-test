bdef06103440fb52c2a607f6f1a4bfd37ac5a36e
PriyaDharshini_Kathiravan <priyakathiravan05@gmail.com>
2025-08-28 16:19:49 +0000

    RDK-58749:Add new json response and update upload and swupdate scripts for DCM-AGENT L2 testing

--- native-platform/uploadSTBLogs.sh
+++ native-platform/uploadSTBLogs.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 #function for directing to logs to dcmd log file
-
+LOG_PATH="/opt/logs"
 DCM_LOG_FILE=$LOG_PATH/dcmd.log.0
 uploadLog() {
     echo "`/bin/timestamp` : $0: $*" >> $DCM_LOG_FILE
 }
-
-DCM_UPLOAD_LIST="$LOG_PATH/dcm_upload"
-DCM_LOG_PATH="/tmp/DCM"
 
 #input arguments
 
@@ -22,7 +19,19 @@ TriggerType=$7
 RRD_FLAG=$8
 RRD_UPLOADLOG_FILE=$9
 
-if [ "$RRD_FLAG" -eq 1 ]; then
+
+#Function to Upload Logs when Flag is true
+uploadLogOnReboot()
+{
+    uploadLog "Called uploadLogOnReboot with $1"
+}
+
+uploadDCMLogs()
+{
+    uploadLog "Called uploadDCMLogs"
+}
+
+if [ -n "$RRD_FLAG" ] && [ "$RRD_FLAG" -eq 1 ]; then
     RRD_DIR="/tmp/rrd/"
     UploadHttpLink="https://mockxconf:50054/rrdUploadFile"
     ret=`curl -k -F "file=@$RRD_DIR$RRD_UPLOADLOG_FILE" $UploadHttpLink --insecure -w "%{http_code}" -o /dev/null`
@@ -40,9 +49,35 @@ if [ "$RRD_FLAG" -eq 1 ]; then
         exit 127
     fi
 else
-    echo "called /lib/rdk/uploadSTBLogs.sh with arguments flag $FLAG dcm flag $DCM_FLAG UploadOnReboot $UploadOnReboot Protocol $UploadProtocol Link $UploadHttpLink"
+    uploadLog "Triggered uploadSTBLogs.sh with arguments TFTP_SERVER=$TFTP_SERVER,FLAG=$FLAG, DCM_FLAG=$DCM_FLAG, UploadOnReboot=$UploadOnReboot, UploadProtocol=$UploadProtocol, UploadHttpLink= $UploadHttpLink &"
+    #Read Upload_Flag information
+    if [ -f "/tmp/DCMSettings.conf" ]; then
+        upload_flag=`cat /tmp/DCMSettings.conf | grep 'urn:settings:LogUploadSettings:upload' | cut -d '=' -f2 | sed 's/^"//' | sed 's/"$//'`
+        uploadLog "upload_flag = $upload_flag"
+    fi
 
+    #logupload functions
+    if [ $DCM_FLAG -eq 0 ] ; then
+        uploadLog "Uploading Without DCM"
+        uploadLogOnReboot true
+    else
+        if [ $FLAG -eq 1 ] ; then
+            if [ $UploadOnReboot -eq 1 ]; then
+                uploadLog "UploadOnReboot set to true"
+                uploadLogOnReboot true
+            else
+                uploadLog "UploadOnReboot set to false"
+                uploadLogOnReboot false
+            fi
+        else
+            if [ $UploadOnReboot -eq 0 ]; then
+                uploadDCMLogs
+            else
+                uploadDCMLogs
+            fi
+        fi
+    fi
+fi
 
 exit 0
-
 
