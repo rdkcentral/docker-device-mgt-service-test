2a5ff8c6a1bcc6ae0a1abac4389fb87aa7552db4
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-01-06 18:23:21 +0530

    Update test_docker.py

--- test_docker.py
+++ test_docker.py
@@ -46,7 +46,7 @@ def test_ports_are_open_ipv6_mockxconf(container):
         print(f"✅ Port {port} is open in {container.name}")
 
 def test_node_processes_running_mockxconf(container):
-    """Ensure exactly 6 Node.js processes are running inside mockxconf."""
+    """Ensure exactly 8 Node.js processes are running inside mockxconf."""
     if container.name != "mockxconf":
         pytest.skip(f"Skipping Node.js process check for {container.name}")
 
@@ -60,8 +60,8 @@ def test_node_processes_running_mockxconf(container):
     node_process_count = int(output.strip())  # Convert output to integer
     print(f"Found {node_process_count} Node.js processes running in {container.name}")
 
-    assert node_process_count == 6, f"Expected 6 Node.js processes, but found {node_process_count}!"
-    print(f"✅ All 6 Node.js processes are running in {container.name}")
+    assert node_process_count == 8, f"Expected 6 Node.js processes, but found {node_process_count}!"
+    print(f"✅ All 8 Node.js processes are running in {container.name}")
 
 def test_files_exist(container):
     """Verify that expected files exist in the respective container."""
